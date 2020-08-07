0e7df4f061d371f2cd1e7c27bb9de4d2351de0f3
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-07 14:56:50 -0700

    add machine addengineer view

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -3,6 +3,7 @@ using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
